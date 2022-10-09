c388d8afbca445c6b221fc3e123677ee0d92aad0
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-10-09 12:34:23 +0300

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+import { productsURL } from '@constants';
+
 function Home() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h1>Welcome to Freshnesecom!</h1>
+      <Link to={productsURL}>
+        <button>Go to products</button>
+      </Link>
     </div>
   );
 }
