796a8211fa09c80218fe5fbe833f2eb904744f35
Taras Ivanchyshyn <tarasivanchyshyn@gmail.com>
2022-10-11 20:25:40 +0300

    deleted unused code

--- src/store/reducers/ProductsSlice.ts
+++ src/store/reducers/ProductsSlice.ts
@@ -1,35 +1,18 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { IProduct } from 'models/IProduct';
 
 interface ProductsState {
   products: IProduct[];
-  isLoading: boolean;
-  error: string;
 }
 
 const initialState: ProductsState = {
-  products: [],
-  isLoading: false,
-  error: ''
+  products: []
 };
 
 const productsSlice = createSlice({
   name: 'products',
   initialState,
-  reducers: {
-    productsFetching(state) {
-      state.isLoading = true;
-    },
-    productsFetchingSuccesss(state, action: PayloadAction<IProduct[]>) {
-      state.isLoading = false;
-      state.error = '';
-      state.products = action.payload;
-    },
-    productsFetchingError(state, action: PayloadAction<string>) {
-      state.isLoading = false;
-      state.error = action.payload;
-    }
-  }
+  reducers: {}
 });
 
 export const productsActions = productsSlice.actions;
